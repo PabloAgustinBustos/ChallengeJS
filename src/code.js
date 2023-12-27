917f09626a635491ffea637d1f97447e4ba36cb2
Pablo Bustos <pablobustos609@gmail.com>
2023-12-27 17:33:44 -0300

    solve first solution

--- src/code.js
+++ src/code.js
@@ -1,5 +1,14 @@
 export function firstSolution(numbers, result) {
-
+  for (let i =  0; i < numbers.length-1; i++)  {
+    let a = numbers[i]
+    
+    for (let j = i+1; j < numbers.length; j++)  {
+      let b = numbers[j]
+  
+      if(a+b === result)  return  true
+    }
+  }
+  
   return false
 }
 
