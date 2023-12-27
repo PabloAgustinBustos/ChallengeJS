1707c1d852bb17a9307b75c47f74a8540608a744
Pablo Bustos <pablobustos609@gmail.com>
2023-12-27 17:29:30 -0300

    add three tests

--- test/code.test.js
+++ test/code.test.js
@@ -4,5 +4,8 @@ import { describe, it, expect } from "vitest"
 describe("firstSolution", () => {
   it("should return false when function cannot make the result", () => {
     expect(firstSolution([1,4,3,9], 8)).toBe(false)
+    expect(firstSolution([1,2,3,4], 18)).toBe(false)
+    expect(firstSolution([24,79,3,1], 24)).toBe(false)
+    expect(firstSolution([-5,3,-3,-15], 7)).toBe(false)
   })
 })
