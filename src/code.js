f42fcea428e32a4d7742b2c8958541265986b2fd
Pablo Bustos <pablobustos609@gmail.com>
2023-12-27 17:37:22 -0300

    add code for secondSolution

--- src/code.js
+++ src/code.js
@@ -1,11 +1,11 @@
 export function firstSolution(numbers, result) {
-  for (let i =  0; i < numbers.length-1; i++)  {
+  for (let i =  0; i < numbers.length-1; i++) {
     let a = numbers[i]
     
-    for (let j = i+1; j < numbers.length; j++)  {
+    for (let j = i+1; j < numbers.length; j++) {
       let b = numbers[j]
   
-      if(a+b === result)  return  true
+      if(a+b === result) return true
     }
   }
   
@@ -14,5 +14,19 @@ export function firstSolution(numbers, result) {
 
 
 export function secondSolution(numbers, result) {
-  
+  // se guardan los números ya comprobados para ver si coinciden con el complemento de otro (El array no es una opción en esta solución)
+  const cache = new Set()
+
+  for (let number of numbers) {
+    // se calcula el complemento. number + complement = result
+    let complement = result - number
+    
+    // si el complemento es un número ya comprobado significa que se puede formar el result sumando dos elementos
+    if (cache.has(complement)) return true
+
+    // En caso de que no exista en el set, se añade.
+    cache.add(number)
+  }
+
+  return false
 }
